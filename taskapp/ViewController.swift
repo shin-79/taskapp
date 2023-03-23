ae80da0bf5f973394304dacc9793e836b0ac7c5d
kimura.shinnosuke <shiinosuke.kimura79@gmail.com>
2023-03-23 16:14:31 +0900

    pod installと.gitignoreの作成まで実施

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.fillerRowHeight = UITableView.automaticDimension
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    
+    //データの数（=セルの数）を返すメゾット
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    
+    //　各セルの内容を返すメゾット
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能な　cell を得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        
+        return cell
+    }
+    //各セルを選択した時に実行されるメゾット
+    func tableView(_tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    }
+    
+    //セルが削除が可能な事を伝えるメゾット
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath)-> UITableViewCell.EditingStyle {
+            return .delete
+        }
+    
+    //Deleteボタンが押されたときに呼ばれるメゾット
+    func tableView(_tableView: UITableView, commit editingStyle:UITableViewCell.EditingStyle,forRowAt indexPath: IndexPath){
+    }
 }
+    
+
 
